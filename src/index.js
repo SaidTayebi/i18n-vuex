44392a9c2eedbe428547153c5cd25df44f5c013e
Saïd Tayebi <s.tayebi@agilishq.com>
2017-01-02 18:39:27 +0100

    use webpack to build

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
 // import the vuex module for localization
-import i18nVuexModule from './vuex-i18n-store';
+import store from './store';
 
 // import the corresponding plugin for vue
-import VuexI18nPlugin from './vuex-i18n-plugin';
+import plugin from './plugin';
 
 // export both modules as one file
 export default {
-	store: i18nVuexModule,
-	plugin: VuexI18nPlugin
+	store,
+	plugin
 };
